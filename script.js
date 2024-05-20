402c75ef72ac12fa93140f6a78bc0e77ff4fe970
Enric Ledo Muntal <enricledomuntal@gmail.com>
2024-05-20 18:39:55 +0200

    Images to WEBP instead of JPG

--- script.js
+++ script.js
@@ -4,3 +4,36 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+var ln = navigator.language || navigator.userLanguage;
+console.log(`Idioma de l'usuari: ${ln}`);
+
+/*
+CLOUD:
+Firebase,
+AWS,
+Docker Compose
+
+LANGUAGES:
+Java,
+Kotlin, 
+Python,
+C++
+
+FRAMEWORKS:
+Jetpack Compose,
+Spring Boot,
+Thymeleaf, 
+Hibernate,
+JPA
+
+DATABASES:
+MongoDB,
+MySQL,
+
+FRONTEND:
+HTML,
+CSS
+
+VERSION CONTROL:
+Git
+*/
