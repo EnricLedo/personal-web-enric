0ea5e9af2697d6ec5e1fed551b85dda70269393b
Enric Ledo <enricledomuntal@gmail.com>
2024-11-13 17:58:56 +0100

    Encara falta millorar amb quins inputs he de donar la resposta "troll"

--- calculator.js
+++ calculator.js
@@ -2,27 +2,41 @@
 function calculateCompoundInterest() {
     // Retrieve input values from the form
     var principal = parseFloat(document.getElementById("principal").value);
-    var rate = parseFloat(document.getElementById("rate").value) / 100;
+    var anualRate = parseFloat(document.getElementById("rate").value) / 100;
     var months = parseFloat(document.getElementById("months").value);
     var frequency = parseInt(document.getElementById("frequency").value);
     var monthlyAddition = parseInt(document.getElementById("monthly-addition").value);
 
-
-/*
- 10.000€ 10y 10% = 25.937,42€
- 0€ 10y 10% (+10.000€/y) =  17,531.17€
- 10.000€ 10y 10% (+10.000€/y) =  43,468.59€
-*/
-
-
-
-    // Check if any of the inputs are not valid (NaN)
-    if (isNaN(principal) || isNaN(rate) || isNaN(months) || isNaN(frequency)) {
+    if (isNaN(principal)){
+        principal = 0;
+    } 
+    if (isNaN(anualRate)){
+        anualRate = 0;
+    } 
+    if(isNaN(frequency)){
+        frequency = 0;
+    } 
+    if(isNaN(months) || (isNaN(anualRate))) {
         var amount = 420.69; 
         var interest = 420.69;
     } else {
-        amount = principal * Math.pow(1 + rate / frequency, frequency * (months / 12));
-        interest = amount - principal;
+
+        /*
+        Sumem l'interés que s'ha afegit mensualment, 
+        i després li sumem el càlcul de l'interés del capital inicial
+        
+          10.000€ 10y 10% = 25.937,42€
+          0€ 10y 10% (+10.000€/y) =  17,531.17€
+          10.000€ 10y 10% (+10.000€/y) =  43,468.59€
+        */
+        amount = 0;
+        for (let i = 0; i < months; i++) {
+            amount = calculateOneMonthInterest(amount, anualRate);
+            amount += monthlyAddition;
+        }
+
+        amount += principal * Math.pow(1 + anualRate / frequency, frequency * (months / 12));
+        interest = amount - principal -(monthlyAddition * months);
     }
 
     function formatNumber(num) {
@@ -42,4 +56,9 @@ function calculateCompoundInterest() {
         <p style="margin-top: 10px">Final Amount: ${formatNumber(amount)}€</p>
         <p>Total Interest: ${formatNumber(interest)}€</p>
     `;
+
+    function calculateOneMonthInterest(amount, anualRate) {
+        const monthlyRate = Math.pow(1 + anualRate, 1 / 12) - 1;
+        return amount * (1 + monthlyRate);
+    }    
 }
